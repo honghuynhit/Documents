1db8d1072dad7b117133171a20ce47226c7f7e30
honghuynhit <honghuynhit@gmail.com>
2019-04-27 14:24:32 +0700

    fix header and add new function

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: honghuynhit 
  * @Date: 2019-04-26 17:24:58 
- * @Last Modified by:   honghuynhit 
- * @Last Modified time: 2019-04-26 17:24:58 
+ * @Last Modified by: honghuynhit
+ * @Last Modified time: 2019-04-26 21:50:27
  */
 
 import { Component } from '@angular/core';
@@ -19,10 +19,32 @@ export class AppComponent {
 
   //Create new email and binding value when type in input
   email = new FormControl('');
+  listContact = [];
+  contact = {};
+  id: number;
+  
 
   //Update email when click update email
   updateEmail() {
     this.email.setValue('honghuynhit@gmail.com');
+    
+  }
+
+  //Function add contact
+  addContact(){
+    if(!this.email.value){
+      console.log('This value is empty');
+      return;
+    }
+    
+    //check last id;
+    this.id = this.listContact.length;
+
+    //Add address to listContact
+    this.listContact.push(
+      {id: this.id + 1, email: this.email.value}
+    );
+    
   }
 
   //Reset email from form control
@@ -33,6 +55,15 @@ export class AppComponent {
   //Onchange model
   modelChanged(Object) {
     var Object = Object;
+    if(!Object){
+      console.log('This value is empty!');
+      return;
+    }
     console.log(Object);
+  }
+
+  //Reset list contact
+  resetList(){
+    this.listContact.length = 0;
   }
 }
